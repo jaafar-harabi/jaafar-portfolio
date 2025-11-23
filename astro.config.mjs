f872013bbcda5f5ed286c8646e22410ca1514c3c
jaafar-harabi <hjaafar@daytruck.tn>
2025-11-23 19:40:48 +0100

    fix: correct site URL and ensure proper formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://jaafar-harabi.github.io',
-  base: '/jaafar-portfolio',
+  site: 'https://jaafar-harabi.github.io/jaafar-portfolio',
+  base: '/jaafar-portfolio/',
+  integrations: [mdx(), sitemap(), tailwind()],
 });
